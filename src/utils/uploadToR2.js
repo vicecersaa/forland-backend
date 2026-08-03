6a116953063dc3027436fcc6ffeae74eaf788ca7
vicecersaa <vicecersaa@gmail.com>
2026-08-03 19:42:33 +0700

    adding features on r2 resizing

--- src/utils/uploadToR2.js
+++ src/utils/uploadToR2.js
@@ -1,67 +1,36 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
-
+import sharp from "sharp";
 import getR2Client from "./r2.js";
 import generateFileName from "./generateFileName.js";
 
+const uploadToR2 = async (buffer, originalName, mimeType, folder) => {
+  const r2 = getR2Client();
+  const filename = generateFileName(originalName);
+  const key = `${folder}/${filename}`;
 
-const uploadToR2 = async (
+  // Resize kalau gambar
+  let finalBuffer = buffer;
+  if (mimeType.startsWith("image/")) {
+    finalBuffer = await sharp(buffer)
+      .resize(1200, 1200, { fit: "inside", withoutEnlargement: true })
+      .jpeg({ quality: 85 })
+      .toBuffer();
+    mimeType = "image/jpeg";
+  }
 
-    buffer,
+  await r2.send(
+    new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET_NAME,
+      Key: key,
+      Body: finalBuffer,
+      ContentType: mimeType,
+    })
+  );
 
-    originalName,
-
-    mimeType,
-
-    folder
-
-) => {
-
-
-    const r2 = getR2Client();
-
-
-    const filename =
-        generateFileName(originalName);
-
-
-    const key =
-        `${folder}/${filename}`;
-
-
-
-    await r2.send(
-
-        new PutObjectCommand({
-
-            Bucket:
-                process.env.R2_BUCKET_NAME,
-
-            Key:
-                key,
-
-            Body:
-                buffer,
-
-            ContentType:
-                mimeType
-
-        })
-
-    );
-
-
-
-    return {
-
-        key,
-
-        url:
-            `${process.env.R2_PUBLIC_URL}/${key}`
-
-    };
-
-
+  return {
+    key,
+    url: `${process.env.R2_PUBLIC_URL}/${key}`,
+  };
 };
-
 
 export default uploadToR2;
