dce7123e36439afe98f126ba746f3bb64bd3dbc8
vicecersa <vicecersaa@gmail.com>
2026-08-04 16:27:07 +0700

    fixing bug on auth middleware and role middleware

--- src/middleware/role.middleware.js
+++ src/middleware/role.middleware.js
@@ -3,6 +3,13 @@ import ApiError from "../utils/ApiError.js";
 const authorize = (...roles) => {
     return (req, res, next) => {
 
+        console.log("ROLE DEBUG:", {
+            rolesAllowed: roles,
+            userId: req.user?._id,
+            email: req.user?.email,
+            role: req.user?.role,
+        });
+
         if (!req.user) {
             return next(new ApiError(401, "Unauthorized"));
         }
