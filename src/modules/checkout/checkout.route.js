7596ab818788ba423026f29657e4eb2974ba667d
vicecersa <vicecersaa@gmail.com>
2026-08-05 04:10:53 +0700

    fix bug

--- src/modules/checkout/checkout.route.js
+++ src/modules/checkout/checkout.route.js
@@ -1,21 +1,10 @@
 import { Router } from "express";
-
 import checkoutController from "./checkout.controller.js";
-import orderController from "../order/order.controller.js";
-
 import authMiddleware from "../../middleware/auth.middleware.js";
 import validate from "../../middleware/validate.middleware.js";
-
-import {
-    createCheckoutSchema
-} from "./checkout.validation.js";
+import { createCheckoutSchema } from "./checkout.validation.js";
 
 const router = Router();
-
-router.get("/check-ongkir", orderController.checkOngkir);
-router.post("/pending-ongkir", orderController.createPendingOngkirOrder);
-router.get("/validate-token", orderController.validateCheckoutToken);
-router.get("/:id/ongkir-status", orderController.checkOngkirStatus);
 
 router.post(
     "/",
